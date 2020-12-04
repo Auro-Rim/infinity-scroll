03ff467fe64760b36447ef81daef82bf08fc9388
Denis Mazurchuk <denis.mazurchuk@gmail.com>
2020-12-04 11:55:16 +0200

    final commit

--- script.js
+++ script.js
@@ -1,15 +1,100 @@
-const exampleArray = [1, 2, [3, 4, [5, 6, 7], 8], 9, 10];
+// const exampleArray = [1, 2, [3, 4, [5, 6, 7], 8], 9, 10];
 
-const flatter = (acc) => {
-  const newArray = acc.reduce((acc, item) => {
-    if (Array.isArray(item)) {
-      acc = acc.concat(flatter(item));
-    } else {
-      acc.push(item);
-    }
-    return acc;
-  }, []);
-  return newArray;
-};
+// const flatter = (acc) => {
+//   const newArray = acc.reduce((acc, item) => {
+//     if (Array.isArray(item)) {
+//       acc = acc.concat(flatter(item));
+//     } else {
+//       acc.push(item);
+//     }
+//     return acc;
+//   }, []);
+//   return newArray;
+// };
 
-console.log(flatter(exampleArray));
+// console.log(flatter(exampleArray));
+
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+let photosArray = [];
+
+let initialLoad = true;
+
+// Unsplash API
+let count = 5;
+const apiKey = "0EErK9lAtrQJMo5cK8gksjwWxarKpXsBvfayacps7qE";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    initialLoad = false;
+    count = 10;
+  }
+}
+
+// Helper Function to set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Elements for Links and Photos, add to DOM
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    img.addEventListener("load", imageLoaded);
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Get photos from Unsplash
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    throw new Error("Something went wrong...");
+  }
+}
+
+// Check to see if scrolling near bottom of page, load more photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// On load
+getPhotos();
